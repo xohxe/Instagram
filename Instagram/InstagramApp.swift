52713e0e1d2ca8c307d83d7c9d3aac4ca791d1d5
xohxe <saeyan2477@gmail.com>
2023-12-29 11:27:57 +0900

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by 김소혜 on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
